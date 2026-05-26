4dfb4567695cb1a5c1e3eb37b41a87c7bd56b502
Vargas0fficial <mbvargas19@gmail.com>
2026-05-26 14:14:18 +0800

    build: fix block root inside mongodb utility fallback config

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,8 @@
-// src/lib/db.ts
+// src/lib/mongodb.ts
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "";
 
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
-}
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://mbvargas91_db_user:Markminard1@cluster0.2oeqdzm.mongodb.net/barangay_db?appName=Cluster0";
 
 let cached = (global as any).mongoose;
 
